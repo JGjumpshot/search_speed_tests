2a04fa4d152c17e5639da4ae2b6b893b4ea868eb
Jackson Gunther <jackson.gunther@yahoo.com>
2021-09-08 20:49:46 -0600

    odd length list

--- search.py
+++ search.py
@@ -4,7 +4,7 @@ SEED_VALUE = int( 100 * random.random())
 # random.seed(SEED_VALUE)
 lyst = sorted(random.sample(range(1000000), k=SEED_VALUE))
 #test.sort()
-print(sorted(lyst), '\n')
+#print(sorted(lyst), '\n')
 
 def linear_search(my_list, target):
     for i in range(len(my_list)):
@@ -17,10 +17,12 @@ def linear_search(my_list, target):
 # def jump_search(lyst, target):
 
 def main():
-    print(lyst, '\n')
+    #print(lyst, '\n')
+    print((len(lyst) / 2))
     linear_search(lyst, lyst[-1])
     linear_search(lyst, lyst[0])
-    linear_search(lyst, (lyst // 2) - 1)
+    print(len(lyst))
+    linear_search(lyst, len(lyst) // 2)
     linear_search(lyst, lyst * 4)
 
 if __name__ == "__main__":
