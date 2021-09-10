ec551577c8be9cc4bddac92ffc734506a625b0f3
Jackson Gunther <jackson.gunther@yahoo.com>
2021-09-10 17:07:09 -0600

    binary search stuff

--- search.py
+++ search.py
@@ -4,20 +4,26 @@ SEED_VALUE = int( 100 * random.random())
 # random.seed(SEED_VALUE)
 lyst = sorted(random.sample(range(1000000), k=SEED_VALUE))
 #test.sort()
-#print(sorted(lyst), '\n')
-
+print(sorted(lyst), '\n')
+print(len(sorted(lyst)), '\n')
 def linear_search(my_list, target):
     for i in range(len(my_list)):
         if my_list[i] == target:
             return True
 
     return False
+#TODO: Need to test if target < my_list[middle]
 def binary_search(my_list, target):
+    new_list = []
     middle = (len(my_list) // 2) - 1
-    print(my_list[middle])
-    # if (my_list):
-    #     return True
-    # elif (target != my_list[i]):
+
+    print(f"{my_list}\n{my_list[middle]} \n{my_list[middle:]}")
+    if (target > my_list[middle]):
+        new_list.append(my_list[middle:])
+        return True
+    else:
+        return False
+print(binary_search([1,2,3,4,5,6,7,8], 5))
 
 
 # def jump_search(lyst, target):
